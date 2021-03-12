cdcc8ed017900f3e9e1cc585bb1cf4cb3114ec72
Gaurav Mehta <gaurav@rancher.com>
2021-03-12 12:07:37 +1100

    vault and glue integration

--- import-cluster/cloud-init.tpl
+++ import-cluster/cloud-init.tpl
@@ -3,5 +3,36 @@ resize_rootfs: true
 hostname: ${node_name}
 ssh_authorized_keys:
   - ${ssh_key}
+write_files:
+- content: |
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+      name: vault-glue-operator
+    ---
+    apiVersion: v1
+    kind: Secret
+    metadata:
+      name: vault-token
+      namespace: vault-glue-operator
+    type: Opaque
+    data:
+      token: ${vault_token}
+    ---
+    apiVersion: vault.cattle.io/v1alpha1
+    kind: Register
+    metadata:
+      name: external-secrets
+    spec:
+      vaultAddr: ${vault_addr}
+      serviceAccount: external-secrets-kubernetes-external-secrets
+      namespace: kube-external-secrets
+      sslDisable: true
+      vaultPolicy:
+        - fleet-demo
+      roleName: fleet-demo
+  path: /var/lib/rancher/k3s/server/manifests/vaultToken.yaml
 runcmd:
-  - 'curl  -sfL https://get.k3s.io  | sh -'
+  - sudo mkdir -p /var/lib/rancher/k3s/server/manifests
+  - sudo wget --no-check-certificate --output-document /var/lib/rancher/k3s/server/manifests/import.yaml ${manifest_url}
+  - 'curl  -sfL https://get.k3s.io  | INSTALL_K3S_CHANNEL=${channel} sh -'
--- import-cluster/main.tf
+++ import-cluster/main.tf
@@ -7,6 +7,14 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
+provider "rancher2" {
+  api_url    = var.rancher2_url
+  access_key = var.rancher2_access_key
+  secret_key = var.rancher2_secret_key
+}
+
+provider "vault" {
+}
 
 variable "vsphere_server" {}
 variable "vsphere_user" {}
@@ -16,6 +24,16 @@ variable "vsphere_dc" {}
 variable "vsphere_datastore" {}
 variable "vsphere_resource_pool" {}
 variable "vsphere_network" {}
+variable "rancher2_url" {}
+variable "rancher2_access_key" {}
+variable "rancher2_secret_key" {}
+variable "vault_addr" {}
+variable "cluster_count" {
+  default = 3
+}
+variable "channel" {
+  default = "v1.19"
+}
 
 data "vsphere_datacenter" "dc" {
   name = var.vsphere_dc
@@ -41,11 +59,24 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+resource "rancher2_cluster" "fleet-k3s-imported" {
+  name = "fleet-k3s-${count.index+1}"
+  count = var.cluster_count
+  description = "fleet k3s import"
+  labels = tomap({"envType" = "dev", "secrets" = "enabled" })
+}
+
+resource "vault_token" "fleet-k3s-register" {
+  count = var.cluster_count
+  renewable = false
+  ttl = "15m"
+}
+
 resource "vsphere_virtual_machine" "k3s" {
   name             = "fleet-k3s-${count.index+1}"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
-  count = 1
+  count = var.cluster_count
   num_cpus  = 4
   memory    = 8196
   guest_id  = data.vsphere_virtual_machine.template.guest_id
@@ -67,7 +98,7 @@ resource "vsphere_virtual_machine" "k3s" {
 
   vapp {
     properties = {
-      user-data = "${base64encode(templatefile("${path.module}/cloud-init.tpl", {node_name = "fleet-k3s-${count.index+1}", ssh_key = file(var.ssh_id) }))}"
+      user-data = "${base64encode(templatefile("${path.module}/cloud-init.tpl", {node_name = "fleet-k3s-${count.index+1}", ssh_key = file(var.ssh_id),manifest_url = rancher2_cluster.fleet-k3s-imported[count.index].cluster_registration_token[0].manifest_url, vault_token = base64encode(vault_token.fleet-k3s-register[count.index].client_token), vault_addr = var.vault_addr, channel = var.channel }))}"
       hostname = "fleet-k3s-${count.index+1}"
     }
   }
